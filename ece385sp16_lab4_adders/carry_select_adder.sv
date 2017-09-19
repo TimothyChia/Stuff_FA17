425244e277951da994faee5beb491eb3acbcb5c1
samhung19 <sam.hung197@gmail.com>
2017-09-18 23:04:48 -0500

    done??!!

--- ece385sp16_lab4_adders/carry_select_adder.sv
+++ ece385sp16_lab4_adders/carry_select_adder.sv
@@ -1,53 +1,51 @@
-// module carry_select_adder
-// (
-//     input   logic[15:0]     A,
-//     input   logic[15:0]     B,
-//     output  logic[15:0]     Sum,
-//     output  logic           CO
-// );
-//
-//     /* TODO
-//      *
-//      * Insert code here to implement a carry select.
-//      * Your code should be completly combinational (don't use always_ff or always_latch).
-//      * Feel free to create sub-modules or other files. */
-//
-// endmodule
-//
-//
-//
-// 
-// //////here is what i have so far:
-//
-// module adder_carry_select (
-// 	input logic Clk, Reset, Load_B, Run,
-// 	input logic [15:0] SW,
-// 	output logic [15:0] Sum,
-// 	output logic CO,
-// 	output logic [6:0] Ahex0, Ahex1, Ahex2, Ahex3, Bhex0, Bhex1, Bhex2, Bhex3);
-//
-//  //other variables below
-//  logic [15:0] A, B;
-//  HexDriver HA0(SW[15:12], Ahex0);
-//  HexDriver HA1(SW[11:8], Ahex1);
-//  HexDriver HA2(SW[7:4], Ahex2);
-//  HexDriver HA3(SW[3:0], Ahex3);
-//
-//  HexDriver HB0(SW[15:12], Bhex0);
-//  HexDriver HB1(SW[11:8], Bhex1);
-//  HexDriver HB2(SW[7:4], Bhex2);
-//  HexDriver HB3(SW[3:0], Bhex3);
-//
-//  if(load_A)
-// 	assign A[15:0] = SW[15:0];
-//
-//  if(load_B)
-// 	assign B[15:0] = SW[15:0];
-//
-// full_adder_4 F4_0(.A(A[15:12]), .B(B[15:12]), .c_in)
-// full_adder_4 F4_1()
-// full_adder_4 F4_2()
-// full_adder_4 F4_3()
-//
-//
-// endmodule
+module adder_carry_select (
+	input logic [15:0] A,B
+	output logic [15:0] Sum,
+	output logic CO, // oh, not zero.
+	);
+	
+	//would be better to use a packed array for these.
+logic c0_4, c0_8, c0_12, c0_16; //these are the carryouts of the 4bit adders when c_in = 0
+logic [15:0] Sum0; //sum when c_in = 0
+
+logic c1_4, c1_8, c1_12, c1_16; //these are the carryouts of the 4bit adders when c_in = 1	
+logic [15:0] Sum1; //sum when c_in = 1
+//assume c_in is 0	
+
+logic c_4, c_8, c_12, c_16; // used to get the actual c_in for each 4bit adder
+
+logic m_out0, m_out1, m_out2; //output from mux
+//what is operator precedence
+always_comb 
+begin
+ c_4 = c0_4;
+ c_8 = c0_8   | (c1_8&c_4);
+ c_12 = c0_12 | (c1_12&c_8);
+ c_16 = c0_16 | (c1_16&c_12);
+ CO = c_16;
+
+//assume Cin is 0 so don't build extra adder and mux
+full_adder_4 FA0_0(.A(A[3:0]), .B(B[3:0]), .c_in(0), .S(Sum0[3:0]), .c_out(c0_4));
+//full_adder_4 FA1_0(.A(A[3:0]), .B(B[3:0]), .c_in(1), .S(Sum1[3:0]), .c_out(c1_4));
+
+
+full_adder_4 FA0_1(.A(A[7:4]), .B(B[7:4]), .c_in(c4), .S(Sum0[7:4]), .c_out(c0_8));
+full_adder_4 FA1_1(.A(A[7:4]), .B(B[7:4]), .c_in(c4), .S(Sum1[7:4]), .c_out(c1_8));
+
+full_adder_4 FA0_2(.A(A[11:8]), .B(B[11:8]), .c_in(c8), .S(Sum0[11:8]), .c_out(c0_12));
+full_adder_4 FA1_2(.A(A[11:8]), .B(B[11:8]), .c_in(c8), .S(Sum1[11:8]), .c_out(c1_12));
+
+full_adder_4 FA0_3(.A(A[15:12]), .B(B[15:12]), .c_in(c12), .S(Sum0[15:12]), .c_out(c0_16));
+full_adder_4 FA1_3(.A(A[15:12]), .B(B[15:12]), .c_in(c12), .S(Sum1[15:12]), .c_out(c1_16));
+
+//multiplexers should really be in a for loop
+//mux_2to1_4 MUX0 (.sum0(Sum0[3:0]), .sum1(Sum1[3:0]), .sel(0), .out());
+Sum[3:0]=Sum0[3:0]; //since the LSB mux was commented out
+mux_2to1_4 MUX1 (.sum0(Sum0[7:4]), .sum1(Sum1[7:4]), .sel(c_4), .out(Sum[7:4]));
+mux_2to1_4 MUX2 (.sum0(Sum0[11:8]), .sum1(Sum1[11:8]), .sel(c_8), .out(Sum[11:8]));
+mux_2to1_4 MUX3 (.sum0(Sum0[15:12]), .sum1(Sum1[15:12]), .sel(_12), .out(Sum[15:12])); 
+
+
+end
+
+endmodule
